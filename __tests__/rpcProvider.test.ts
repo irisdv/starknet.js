9f0f61d2d7d9e7a24a18956f4965954ab7fc7352
Iris <iris.devillars@gmail.com>
2022-12-09 17:21:25 +0100

    test: add getEstimateFee test

--- __tests__/rpcProvider.test.ts
+++ __tests__/rpcProvider.test.ts
@@ -1,6 +1,9 @@
 import { Account, GetBlockResponse, RpcProvider, ec } from '../src';
 import { StarknetChainId } from '../src/constants';
+import { encodeShortString } from '../src/utils/shortString';
+import { fromCallsToExecuteCalldata } from '../src/utils/transaction';
 import {
+  compiledErc20,
   compiledOpenZeppelinAccount,
   describeIfNotDevnet,
   describeIfRpc,
@@ -88,6 +91,7 @@ describeIfRpc('RPCProvider', () => {
     describe('deploy contract related tests', () => {
       let contract_address;
       let transaction_hash;
+      let erc20ContractAddress: string;
 
       beforeAll(async () => {
         const { deploy } = await account.declareDeploy({
@@ -99,6 +103,17 @@ describeIfRpc('RPCProvider', () => {
 
         contract_address = deploy.contract_address;
         transaction_hash = deploy.transaction_hash;
+
+        const { deploy: deployErc20 } = await account.declareDeploy({
+          contract: compiledErc20,
+          classHash: '0x54328a1075b8820eb43caf0caa233923148c983742402dcfc38541dd843d01a',
+          constructorCalldata: [
+            encodeShortString('Token'),
+            encodeShortString('ERC20'),
+            contract_address,
+          ],
+        });
+        erc20ContractAddress = deployErc20.contract_address;
       });
 
       test('declareDeploy()', () => {
@@ -119,6 +134,27 @@ describeIfRpc('RPCProvider', () => {
       xtest('traceTransaction', async () => {
         await rpcProvider.traceTransaction(transaction_hash);
       });
+
+      test('getEstimateFee', async () => {
+        const call = {
+          contractAddress: erc20ContractAddress,
+          entrypoint: 'transfer',
+          calldata: [erc20ContractAddress, '10', '0'],
+        };
+        const calldata = fromCallsToExecuteCalldata([call]);
+
+        const estimate = await rpcProvider.getEstimateFee(
+          {
+            contractAddress: contract_address,
+            calldata,
+          },
+          { nonce: 0 }
+        );
+
+        expect(estimate).toHaveProperty('overall_fee');
+        expect(estimate).toHaveProperty('gas_consumed');
+        expect(estimate).toHaveProperty('gas_price');
+      });
     });
 
     test('getClass classHash 0x03fcbf77b28c96f4f2fb5bd2d176ab083a12a5e123adeb0de955d7ee228c9854', async () => {
@@ -128,6 +164,24 @@ describeIfRpc('RPCProvider', () => {
       expect(contractClass).toHaveProperty('program');
       expect(contractClass).toHaveProperty('entry_points_by_type');
     });
+
+    // describe('estimate and invoke related tests', () => {
+    //   let erc20ContractAddress: string;
+
+    //   beforeAll(async () => {
+    //     const { deploy } = await account.declareDeploy({
+    //       contract: compiledErc20,
+    //       classHash: '0x54328a1075b8820eb43caf0caa233923148c983742402dcfc38541dd843d01a',
+    //       constructorCalldata: [
+    //         encodeShortString('Token'),
+    //         encodeShortString('ERC20'),
+    //         account.address,
+    //       ],
+    //     });
+    //     erc20ContractAddress = deploy.contract_address;
+    //   });
+
+    // });
 
     test.todo('getEstimateFee');
 
